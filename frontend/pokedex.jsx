d129c3896e98c21333b2ed9ce97a78b7da3b6083
a-gehlot <andrew.gehlot@gmail.com>
2022-11-20 21:25:13 -0500

    configure root element of react

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById("root");
+    const root = ReactDOM.createRoot(container);
+    root.render(<h1>Pokedex</h1>)
+})
+
